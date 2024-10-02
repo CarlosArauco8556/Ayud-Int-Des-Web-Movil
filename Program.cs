28e787688256e3c67553b9a83cf12cc02199b439
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-10-02 01:04:37 -0300

    feat: add repositoy pattern

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using api.src.Data;
+using api.src.Interfaces;
+using api.src.Repository;
 using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +12,7 @@ Env.Load();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 string connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? "Data Source=app.db";
 builder.Services.AddDbContext<ApplicationDBContext>(opt => opt.UseSqlite(connectionString));
